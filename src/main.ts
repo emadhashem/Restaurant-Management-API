8424deb2834495b5fde0fa952af7efc47ff1e4bd
EmadHashem-im <emad.hashem@imperialstud.io>
2025-10-19 15:43:29 +0300

    save

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v1');
-  // Enforces validation rules on all incoming client payloads
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
